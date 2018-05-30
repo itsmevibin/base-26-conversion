19b50aa518144fcceb26d6b60058dd26a60210ea
vibindas <vibindas.m@mobme.in>
2018-05-30 18:19:01 +0530

    clean up

--- app/src/main/java/com/equipe/base26/MainActivity.kt
+++ app/src/main/java/com/equipe/base26/MainActivity.kt
@@ -5,7 +5,6 @@ import android.support.v7.app.AppCompatActivity
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
-import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,7 +15,6 @@ class MainActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
-    Toast.makeText(this, "welcome.", Toast.LENGTH_LONG).show()
     button.setOnClickListener({
       getActualValue(editText.text.toString())
     })
